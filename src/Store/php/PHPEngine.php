346508d499a06cee08387c1f45d187ed05d940b1
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-02 21:35:29 -0300

    Alter var includes

--- src/Store/php/PHPEngine.php
+++ src/Store/php/PHPEngine.php
@@ -5,7 +5,9 @@ namespace TMPHP\Cache\Store;
 class PHPEngine extends EngineBase implements EngineModel
 {
     protected $content  = null;
-    protected $includes = 'includes/php-simples.php';
+    protected $includes = [
+        'includes/php-simples.php'
+    ];
 
     public function __construct(string $content) {
         $this->content = $content;
