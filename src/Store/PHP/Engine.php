5cb0498fd4fbcbd08a1c1c575ab4149aa479adfa
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-08 20:24:34 -0300

    Remove method construct from Engine

--- src/Store/PHP/Engine.php
+++ src/Store/PHP/Engine.php
@@ -9,9 +9,4 @@ class Engine extends EngineBase implements EngineModel
         'includes/php-simples.php'
     ];
 
-    public function __construct(string $content) {
-        $this->content = $content;
-        $this->content = str_replace('test', 'Test', $this->content);
-    }
-
 }
